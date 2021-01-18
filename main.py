d68f4db525c43d08d5049f2bb8ae325ffefab806
Jia Yuan Teoh <yuan@jias-air.myfiosgateway.com>
2021-01-17 21:10:53 -0500

    revert

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from flask import Flask, request
 import torch
-from transformers import BertTokenizer, BertForQuestionAnswering, BertTokenizerFast, EncoderDecoderModel
+from transformers import BertTokenizer, BertForQuestionAnswering
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-tokenizer = BertTokenizer.from_pretrained('mrm8488/bert-tiny-finetuned-squadv2')
-model = BertForQuestionAnswering.from_pretrained('mrm8488/bert-tiny-finetuned-squadv2')
-tokenizerSum = BertTokenizerFast.from_pretrained('mrm8488/bert-mini2bert-mini-finetuned-cnn_daily_mail-summarization')
-modelSum = EncoderDecoderModel.from_pretrained('mrm8488/bert-mini2bert-mini-finetuned-cnn_daily_mail-summarization')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForQuestionAnswering.from_pretrained('mrm8488/bert-medium-finetuned-squadv2')
 
 @app.route("/")
 def hello_world():
@@ -31,20 +29,6 @@ def bertqa():
     p_end = torch.max(end_scores).item()
     all_tokens = tokenizer.convert_ids_to_tokens(inputs.input_ids[0])
     ans_string = ' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1]).replace("[CLS]", "").replace("[SEP]","").replace(" ##","")
-
-    return { 'answer': ans_string }
-
-@app.route('/bertsum', methods=['POST'])
-def bertsum():
-    request_json = request.get_json()
-    text = request_json['context']
-
-    inputs = tokenizerSum([text], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
-    input_ids = inputs.input_ids
-    attention_mask = inputs.attention_mask
-
-    output = modelSum.generate(input_ids, attention_mask=attention_mask)
-    ans_string = tokenizerSum.decode(output[0], skip_special_tokens=True)
 
     return { 'answer': ans_string }
 
